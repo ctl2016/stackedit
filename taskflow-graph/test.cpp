42398c0017fccefd5336190f9237c53bcbdf9084
ctl2022 <12218668+ctl2022@user.noreply.gitee.com>
2023-10-17 08:11:49 +0000

    update taskflow-graph/test.cpp.
    Signed-off-by: ctl2022 <12218668+ctl2022@user.noreply.gitee.com>

--- taskflow-graph/test.cpp
+++ taskflow-graph/test.cpp
@@ -1,87 +1,95 @@
 #include <iostream>
 #include <unistd.h>
+#include <atomic>
 #include <taskflow/taskflow.hpp>
 
 int main() {
     tf::Taskflow taskflow;
-    tf::Executor executor(8);
-    int i = 0;
+    tf::Executor executor;
+    std::atomic<int> counter(0);
 
     auto initGlobal = taskflow.emplace([] { std::cout << "initGlobal" << std::endl; }).name("initGlobal");
     auto startZmqSvr = taskflow.emplace([] { std::cout << "StartZmqSvr" << std::endl; }).name("StartZmqSvr");
     auto chkOtaEvt = taskflow.emplace([&] { 
-            sleep(1);
-            if(i <= 3) {
-                std::cout << "ChkOtaEvt 0, i:" << i << std::endl;
+            counter.fetch_add(1);
+            int i = counter.load();
+            // sleep(1);
+            if(i <= 5) {
+                std::cout << "ChkOtaEvt (i <= 5), i:" << i << std::endl;
                 return 0;
             }
-            else {
-                std::cout << "ChkOtaEvt 1, i:" << i << std::endl;
+            else if(i == 6) {
+                std::cout << "ChkOtaEvt (i = 6), i:" << i << std::endl;
                 return 1;
             }
-            }).name("chkOtaEvt");
+            else {
+                std::cout << "ChkOtaEvt (i > 6), i:" << i << std::endl;
+            }
+            return 2;
+    }).name("chkOtaEvt");
 
     auto chkActState = taskflow.emplace([] { std::cout << "ChkActState" << std::endl; }).name("chkActState");
 
-    auto startAct = taskflow.emplace(
+    auto activate = taskflow.emplace(
             [&] (tf::Subflow& subflow) {
 
-            auto actSoc = subflow.emplace([&]() {
-                    printf("  Subtask actSoc\n");
+            subflow.emplace([&]() {
+                    std::cout << "  Subtask actSoc\n";
                     }).name("actSoc");
 
-            auto actMcu = subflow.emplace([&]() {
-                    printf("  Subtask actMcu\n");
+            subflow.emplace([&]() {
+                    std::cout << "  Subtask actMcu\n";
                     }).name("actMcu");
 
             // detach or join the subflow (by default the subflow join at B)
             subflow.join();
             //subflow.detach();
             }
-            ).name("startAct");
+            ).name("activate");
 
-    auto startFlash = taskflow.emplace(
+    auto flash = taskflow.emplace(
             [&] (tf::Subflow& subflow) {
 
-                auto flashSoc = subflow.emplace([&]() {
-                        printf("  Subtask flashSoc\n");
-                        }).name("flashSoc");
+                subflow.emplace([&]() {
+                        std::cout << "  Subtask flashSoc\n";
+                }).name("flashSoc");
 
-                auto flashMcu = subflow.emplace([&]() {
-                        printf("  Subtask flashMcu\n");
-                        }).name("flashMcu");
-
+                subflow.emplace([&]() {
+                        std::cout << "  Subtask flashMcu\n";
+                }).name("flashMcu");
+                
                 // detach or join the subflow (by default the subflow join at B)
                 subflow.join();
                 //subflow.detach();
             }
-            ).name("startFlash");
+            ).name("flash");
 
-    auto loopDo = taskflow.emplace([&] { 
-                std::cout << "loopDo, i:" << i << std::endl;
-            }).name("loopDo");
-    
-    auto loopWhile = taskflow.emplace([&] { 
-                ++i;
-                std::cout << "loopWhile, i:" << i << std::endl;
+    auto endFlash = taskflow.emplace([&] { 
+                std::cout << "endFlash\n";
                 return 0;
-            }).name("loopWhile");
+            }).name("endFlash");
 
+    auto endAct = taskflow.emplace([&] { 
+                std::cout << "endAct\n";
+                return 0;
+            }).name("endAct");
 
-    auto reboot = taskflow.emplace([] { std::cout << "Reboot" << std::endl; }).name("reboot");
+    //auto reboot = subflow.emplace([&] { std::cout << "Reboot\n"; }).name("reboot");
+    //reboot.succeed(actSoc, actMcu);
 
     initGlobal.precede(chkActState, startZmqSvr);
-    chkActState.precede(loopDo);
-    loopDo.precede(chkOtaEvt);
-    chkOtaEvt.precede(startFlash, startAct);
-    startAct.precede(reboot);
-    reboot.precede(loopWhile);
-    startFlash.precede(loopWhile);
-    loopWhile.precede(chkOtaEvt);
+    chkActState.precede(chkOtaEvt);
+
+    chkOtaEvt.precede(flash, activate);
+
+    flash.precede(endFlash);
+    activate.precede(endAct);
+    
+    endFlash.precede(chkOtaEvt);
+    endAct.precede(chkOtaEvt);
 
     executor.run(taskflow).get();  // block until finished
     taskflow.dump(std::cout);
 
     return 0;
 }
-
