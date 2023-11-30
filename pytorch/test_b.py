95624e1a6299458984eeb89d3cb590fff3b71912
ctl2022 <12218668+ctl2022@user.noreply.gitee.com>
2023-11-30 09:46:51 +0000

    update pytorch/test_b.py.
    Signed-off-by: ctl2022 <12218668+ctl2022@user.noreply.gitee.com>

--- pytorch/test_b.py
+++ pytorch/test_b.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -14,13 +16,9 @@ train_dataset = torchvision.datasets.MNIST(root='./data', train=True, download=T
 test_dataset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 
 # 定义超参数
-batch_size = 64
-learning_rate = 0.00001
+batch_size = 20
+learning_rate = 0.001
 num_epochs = 10
-
-# 创建数据加载器
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 # 实例化模型
 model = utils.CNNB()
@@ -38,33 +36,43 @@ optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 losses = []
 epoches = []
 
-total_step = len(train_loader)
 for epoch in range(num_epochs):
     running_loss = 0.0
+    steps = []
+    steps_loss = []
+    # 创建数据加载器
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    total_step = len(train_loader)
+    print('train_loader len:', len(train_loader))
+
     for i, (images, labels) in enumerate(train_loader):
         # 前向传播
         outputs = model(images)
         loss = criterion(outputs, labels)
-
         # 反向传播和优化
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
-
-        if (i + 1) % 300 == 0:
+        if (i + 1) % 100 == 0:
+            steps.append(i+1)
+            steps_loss.append(loss.item())
+            utils.plot_loss(steps, steps_loss, True)
             print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{total_step}], Loss: {loss.item()}')
+            utils.plot_loss(epoches, losses)
 
     learing_r = optimizer.param_groups[0]['lr']
     loss_r = running_loss / len(train_loader)
     losses.append(loss_r)
     epoches.append(epoch)
-    utils.plot_loss(epoches, losses)
+    utils.plot_loss(epoches, losses, True)
     print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss_r:.6f}, Learing: {learing_r:.6f}')
 
-
 # 在测试集上评估模型
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
+print('test_loader len:', len(test_loader))
 model.eval()
+
 with torch.no_grad():
     correct = 0
     total = 0
