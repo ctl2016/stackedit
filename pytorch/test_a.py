c8bf6add05ccca6dba2376fa6c5fbb8a7eda2c00
ctl2022 <12218668+ctl2022@user.noreply.gitee.com>
2023-12-18 09:13:42 +0000

    update pytorch/test_a.py.
    Signed-off-by: ctl2022 <12218668+ctl2022@user.noreply.gitee.com>

--- pytorch/test_a.py
+++ pytorch/test_a.py
@@ -1,81 +1,136 @@
-import sys
 import os
+import sys
 import torch
-from torch import nn
-from torch import optim
-import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
 import torchvision
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from torch.optim import lr_scheduler
 import utils
+from PIL import Image, ImageOps
 
-# 定义变换
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
-])
+num_epochs = 16
+batch_size = 32
+learning_rate = 0.001
 
-# 加载 MNIST 数据集
-trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+class AddBorder(object):
+    def __init__(self, border_color, border_size):
+        self.border_color = border_color
+        self.border_size = border_size
 
-# 创建CNN模型实例
+    def __call__(self, img):
+        return ImageOps.expand(img, border=self.border_size, fill=self.border_color)
+
+# 创建一个包含自定义 transform 函数的 transforms.Compose()
+
+custom_transform = AddBorder((0), 3)
+
+# 定义数据变换
+transform = transforms.Compose([transforms.RandomAffine(degrees=45, translate=(0, 0), scale=(0.99, 1.01), shear=50),
+                                custom_transform,
+                                transforms.Resize((28, 28)),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5,), (0.5,))
+                               ])
+
+# 加载训练集和测试集
+train_dataset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+test_dataset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+
+# 实例化模型
 model = utils.CNNA()
 
 # 加载模型权重
-
 if os.path.exists(model.save_name()):
     model.load_state_dict(torch.load(model.save_name()))
 
 # 定义损失函数和优化器
-#criterion = nn.CrossEntropyLoss()
-#optimizer = optim.Adam(model.parameters())
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters())
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.001) # weight_decay = 0.001 使用 L2正则化
 
+def lambda_rule(epoch):
+    curr_lr = optimizer.param_groups[0]['lr']
+    lr_l = 1.0 - epoch / num_epochs
+    print(f'lambda_rule epoch: {epoch}, curr_lr: {curr_lr:.6f}, lr_l: {lr_l:.6f}')
+    return lr_l
+
+scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda_rule)
 # 训练模型
 
 losses = []
 epoches = []
-
-num_epochs = 10
+loss_r = 0
 
 for epoch in range(num_epochs):
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=20, shuffle=True)
-    total_step = len(trainloader)
     running_loss = 0.0
-    print('trainloader len:', len(trainloader))
+    steps = []
+    steps_loss = []
+    # 创建数据加载器
 
-    for i, (images, labels) in enumerate(trainloader):
-        optimizer.zero_grad()
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    total_step = len(train_loader)
+    print('train_loader len:', len(train_loader))
+
+    curr_lr = optimizer.param_groups[0]['lr']
+    
+    for i, (images, labels) in enumerate(train_loader):
+        # 前向传播
         outputs = model(images)
+        #print('labels:', labels)
+        #print('outputs:', outputs)
         loss = criterion(outputs, labels)
+        # 反向传播和优化
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+    
         running_loss += loss.item()
-        if (i + 1) % 300 == 0:
-            print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{total_step}], Loss: {loss.item()}')
+        
+        if (i + 1) % batch_size == 0:
+            steps.append(i+1)
+            steps_loss.append(loss.item())
+            utils.plot_loss(steps, steps_loss, True)
+            print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{total_step}], Learing: {curr_lr:.6f}, Loss: {loss.item()}, Loss_r: {loss_r:.6f}')
+            utils.plot_loss(epoches, losses)
 
-    learing_r = optimizer.param_groups[0]['lr']
-    loss_r = running_loss / len(trainloader)
+    loss_r = running_loss / len(train_loader)
     losses.append(loss_r)
     epoches.append(epoch)
-    utils.plot_loss(epoches, losses)
-    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss_r:.6f}, Learing: {learing_r:.6f}')
+    utils.plot_loss(epoches, losses, True)
+    scheduler.step()
+    print(f'Epoch [{epoch+1}/{num_epochs}], Loss_r: {loss_r:.6f}')
+    torch.save(model.state_dict(), model.save_name())
 
-# 评估模型
-correct = 0
-total = 0
-testloader = torch.utils.data.DataLoader(testset, batch_size=1, shuffle=False)
-print('testloader len:', len(testloader))
+# 在测试集上评估模型
+test_loader = torch.utils.data.DataLoader(test_dataset, 1, shuffle=False)
+print('test_loader len:', len(test_loader))
+model.eval()
+
+test_loss = 0.0
+total_samples = 0
 
 with torch.no_grad():
-    for images, labels in testloader:
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
         outputs = model(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
+        # 计算损失
+        loss = criterion(outputs, labels)
+        
+        # 累加损失值
+        test_loss += loss.item() * images.size(0)
+        total_samples += images.size(0)
 
-print('Test accuracy: %.2f%%' % (100 * correct / total))
+    accuracy = correct / total
+    print(f'Test Accuracy: {accuracy * 100}%')
+    
+    # 计算平均损失
+    average_loss = test_loss / total_samples
+    print("Average Loss on Test Set:", average_loss)
 
-# 保存模型
-torch.save(model.state_dict(), model.save_name())
+#torch.save(model.state_dict(), model.save_name())
