10e446d131c968130a50a78ea47f44492a0caf85
ctl2022 <12218668+ctl2022@user.noreply.gitee.com>
2023-12-18 09:15:05 +0000

    update pytorch/eval_test.py.
    Signed-off-by: ctl2022 <12218668+ctl2022@user.noreply.gitee.com>

--- pytorch/eval_test.py
+++ pytorch/eval_test.py
@@ -3,17 +3,21 @@
 import torch
 import torch.nn as nn
 from torchvision.transforms import ToTensor
-from PIL import Image
+from PIL import Image, ImageOps
 import matplotlib.pyplot as plt
 import utils
 from torchvision import transforms
 
+print(torch.__version__)
+print(torch.cuda.is_available())
+print(torch.cuda.device_count())
+#print(torch.cuda.get_device_name(0))
+
 # 实例化模型类
-model = utils.CNNB()
+model = utils.CNNA()
 
 # 加载模型权重
 model.load_state_dict(torch.load(model.save_name()))
-
 # 设置为推理模式
 model.eval()
 
@@ -31,7 +35,21 @@ plt.imshow(image)
 plt.axis('on')  # 可选：关闭坐标轴
 plt.show()
 
+class AddBorder(object):
+    def __init__(self, border_color, border_size):
+        self.border_color = border_color
+        self.border_size = border_size
+
+    def __call__(self, img):
+        return ImageOps.expand(img, border=self.border_size, fill=self.border_color)
+
+# 创建一个包含自定义 transform 函数的 transforms.Compose()
+custom_transform = AddBorder((0), 3)
+
 transform = transforms.Compose([
+    #transforms.RandomRotation(degrees=50),
+    #transforms.RandomAffine(degrees=45, translate=(0, 0), scale=(0.99, 1.01), shear=50),
+    custom_transform,
     transforms.Resize((28, 28)),
     transforms.ToTensor(),
     transforms.Normalize((0.5,), (0.5,))
@@ -41,7 +59,6 @@ transform = transforms.Compose([
 image = image.convert('L')
 
 image = transform(image).unsqueeze(0)  # 转换为张量并添加批次维度
-
 
 # 进行推理
 with torch.no_grad():
