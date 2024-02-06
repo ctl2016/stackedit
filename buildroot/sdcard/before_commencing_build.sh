831fd6139b7bf2fce17b790be2faef1588d27511
ctl2022 <12218668+ctl2022@user.noreply.gitee.com>
2024-02-06 09:15:15 +0000

    add buildroot/sdcard/before_commencing_build.sh.
    Signed-off-by: ctl2022 <12218668+ctl2022@user.noreply.gitee.com>

---
+++ buildroot/sdcard/before_commencing_build.sh
@@ -0,0 +1,4 @@
+mkdir -p ./output/build/uboot-2022.04/
+mkdir -p ./output/build/linux-5.15.18/
+cp -v ./backup_config/uboot-2022.04.config ./output/build/uboot-2022.04/.config
+cp -v ./backup_config/linux-5.15.18.config ./output/build/linux-5.15.18/.config
